c1fb5d1ecf106a8cc3ca7fd6511dd17b0b51bea5
root <root@localhost.localdomain>
2022-10-24 20:42:26 +0900

    auto

--- src/app/share/msstit/msstit.component.ts
+++ src/app/share/msstit/msstit.component.ts
@@ -1,6 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Component, Inject, OnInit, ChangeDetectorRef, HostListener } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormGroup, FormBuilder, FormControl, FormArray, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { UserService } from './../../services/user.service';
+import { GcdhelpComponent } from './../gcdhelp/gcdhelp.component';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-msstit',
@@ -9,43 +14,80 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 })
 export class MsstitComponent implements OnInit {
   mcode: number;
-  mode:number=3;
+  mode: number = 2;
   form: FormGroup;
+  dataSource = new MatTableDataSource();
+  displayedColumns = ['line', 'patno'];
   constructor(private fb: FormBuilder,
-              @Inject(MAT_DIALOG_DATA) data,
-              private dialogRef: MatDialogRef<MsstitComponent>) { 
-                this.mcode = data.mcode;
-              }
+    public usrsrv: UserService,
+    public cdRef: ChangeDetectorRef,
+    @Inject(MAT_DIALOG_DATA) data,
+    private dialogRef: MatDialogRef<MsstitComponent>) {
+    this.mcode = data.mcode;
+  }
 
   ngOnInit(): void {
-    this.form = this.fb.group({});
+    this.form = this.fb.group({ mtbl: this.fb.array([]) });
+    this.ins_row(1);
   }
-  modeToCre():void {
-    this.mode=1;
-    this.form.reset();
-    this.form.enable();
-    // this.edaOld=+this.eda;
-    // this.eda="新規登録";
-    // const bikou={nbikou:this.edasrv.adrs[0].nbikou,
-    //              sbikou:this.edasrv.adrs[0].sbikou,
-    //              obikou:this.edasrv.adrs[0].obikou};
-    // this.form.get('addr').patchValue(bikou);
+  async newPno(i: number) {
+    let lcpno: number = await this.usrsrv.getNumber('patno', 1,);
+    this.frmArr.controls[i].patchValue({ patno: lcpno });
+    this.refresh();
   }
-  modeToUpd():void {
-    this.mode=2;
-    this.form.enable();
+  copyPno(i: number) {
+    if (i > 0) {
+      this.frmArr.controls[i].patchValue({ patno: this.frmArr.getRawValue()[i - 1]['patno'] });
+    }
+    this.refresh();
   }
-  cancel():void {
-    this.mode=3;
-    this.form.disable();
+  del_row(row: number) {
+    this.frmArr.removeAt(row);
+    this.refresh();
   }
-  save():void {
-    this.mode=3;
+  ins_row(row: number) {
+    this.frmArr.insert(row, this.fb.group({
+      line: [{ value: 0, disabled: true }],
+      patno: [{ value: null, disabled: true }]
+    }));
+    this.refresh();
+  }
+  get frmArr(): FormArray {
+    return this.form.get('mtbl') as FormArray;
+  }
+  modeToUpd(): void {
+    this.mode = 2;
+    this.refresh();
+  }
+  cancel(): void {
+    this.mode = 3;
+    this.form.markAsPristine();
+    this.refresh();
+  }
+  refresh(): void {
+    this.dataSource.data = this.frmArr.controls;
+    if (this.mode == 3) {
+      this.form.disable();
+    } else {
+      this.form.enable();
+    }
+    this.cdRef.detectChanges();
+  }
+  save(): void {
+    this.mode = 3;
     this.form.disable();
     this.form.markAsPristine();
   }
   close() {
     this.dialogRef.close();
   }
-
+  shouldConfirmOnBeforeunload(): boolean {
+    return this.form.dirty;
+  }
+  @HostListener('window:beforeunload', ['$event'])
+  beforeUnload(e: Event) {
+    if (this.shouldConfirmOnBeforeunload()) {
+      e.returnValue = true;
+    }
+  }
 }
