29873290535bfb6932d3037c772be2d4fb1fdaa5
mwjsystem <system@mwjapan.jp>
2026-09-20 00:43:45 +0000

    v19update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -25,7 +25,7 @@ export class NavbarComponent implements OnInit {
       this.db = 'テスト環境';
     }
 	this.navsrv.getNavmenu();
-	console.log('navmenu',this.navsrv.menu);
+	console.log('navmenu afterGet',this.navsrv.menu);
     // const color: string = localStorage.getItem(this.usrsrv.userInfo['nickname'] + 'MWSYS_COLOR');
     // console.log(color)
     // if (color !== null) { this.setColor(color); }
